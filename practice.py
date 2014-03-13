7ac571f7cec5cbae87577e53f8903bc40938efcd
Saurabh Sakpal <sfstoe@gmail.com>
2014-03-13 10:07:53 +0530

    Sub Segment generation

--- practice.py
+++ practice.py
@@ -1,3 +1,21 @@
 import sys
-print sys.argv
-
+li = sys.argv
+S = li[1]
+wo = S.split(" ")
+words = [];
+for i in range(len(wo)):
+    if len(wo[i])>0:
+        words.append(wo[i]);
+print words
+subseg = [];
+for i in range(len(words)):
+    temp = words[i];
+    subseg.append(words[i])
+    j = 1;
+    while (i+j)<len((words)):
+        temp = temp+" "+words[i+j];
+        j = j+1
+        subseg.append(temp)
+print subseg
+    
+        
